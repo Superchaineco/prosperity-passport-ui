75c6ccdaef8139c92da6dde08b6482859c428115
sebaxor <49530370+sebaxor@users.noreply.github.com>
2025-06-06 12:19:53 -0500

    fix update the divvi suffix

--- src/components/new-safe/create/logic/index.ts
+++ src/components/new-safe/create/logic/index.ts
@@ -58,12 +58,8 @@ export const getSafeDeployProps = async (
   const { data, to } = getSuperChainSetupCallData(safeParams.seed, safeParams.id, safeParams.owners[0])
 
   const divviSuffix = getDataSuffix({
-    consumer: '0xe6D39BB0a25fF3A1918adc57796656173918AfE5',
-    providers: [
-      '0x0423189886d7966f0dd7e7d256898daeee625dca',
-      '0xc95876688026be9d6fa7a7c33328bd013effa2bb',
-      '0x5f0a55fad9424ac99429f635dfb9bf20c3360ab8',
-    ],
+    consumer: '0x49E59F6267401e05b482B0020A1A65d63E782Ea4',
+    providers: ['0x0423189886d7966f0dd7e7d256898daeee625dca', '0xc95876688026be9d6fa7a7c33328bd013effa2bb', '0x7beb0e14f8d2e6f6678cc30d867787b384b19e20'],
   })
 
   const fullData = data + divviSuffix
