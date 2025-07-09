b27e5e0ee8b5d5178472829f538a2739651145e3
sebaxor <49530370+sebaxor@users.noreply.github.com>
2025-07-09 01:03:32 -0500

    feat: add Celo chain configuration to useLoadChains hook

--- src/hooks/loadables/useLoadChains.ts
+++ src/hooks/loadables/useLoadChains.ts
@@ -1,12 +1,64 @@
 import { useEffect } from 'react'
-import { getChainsConfig, setBaseUrl, type ChainInfo } from '@safe-global/safe-gateway-typescript-sdk'
+import { GAS_PRICE_TYPE, getChainsConfig, RPC_AUTHENTICATION, setBaseUrl, type ChainInfo } from '@safe-global/safe-gateway-typescript-sdk'
 import useAsync, { type AsyncResult } from '../useAsync'
 import { logError, Errors } from '@/services/exceptions'
+
+
+
+const celoChain: ChainInfo = {
+  chainId: '42220',
+  chainName: 'Celo',
+  description: 'Celo mainnet',
+  chainLogoUri: 'https://cryptologos.cc/logos/celo-celo-logo.png',
+  l2: false,
+  isTestnet: false,
+  nativeCurrency: {
+    name: 'CELO',
+    symbol: 'CELO',
+    decimals: 18,
+    logoUri: 'https://cryptologos.cc/logos/celo-celo-logo.png',
+  },
+  transactionService: '',
+  blockExplorerUriTemplate: {
+    address: 'https://explorer.celo.org/address/{{address}}',
+    txHash: 'https://explorer.celo.org/tx/{{txHash}}',
+    api: '',
+  },
+  disabledWallets: [],
+  ensRegistryAddress: undefined,
+  features: [],
+  gasPrice: [
+    {
+      type: GAS_PRICE_TYPE.FIXED,
+      weiValue: '1000000000',
+    },
+  ],
+  publicRpcUri: {
+    authentication: RPC_AUTHENTICATION.NO_AUTHENTICATION,
+    value: 'https://forno.celo.org',
+  },
+  rpcUri: {
+    authentication: RPC_AUTHENTICATION.API_KEY_PATH,
+    value: 'https://celo-mainnet.infura.io/v3/',
+  },
+  safeAppsRpcUri: {
+    authentication: RPC_AUTHENTICATION.API_KEY_PATH,
+    value: 'https://celo-mainnet.infura.io/v3/',
+  },
+  shortName: 'celo',
+  theme: {
+    textColor: '#ffffff',
+    backgroundColor: '#35D07F',
+  }
+}
+
 
 const getConfigs = async (): Promise<ChainInfo[]> => {
   setBaseUrl('https://safe-client.safe.global')
   const data = await getChainsConfig()
-  return data.results || []
+  const results = data.results || []
+  const hasCelo = results.some((c) => c.chainId === '42220')
+  return hasCelo ? results : [...results, celoChain]
 }
 
 export const useLoadChains = (): AsyncResult<ChainInfo[]> => {
