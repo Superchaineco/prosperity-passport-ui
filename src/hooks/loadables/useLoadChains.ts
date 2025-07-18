a4e2e41dca59169b31dbcdbd9c89b9bf9dcc2018
anotherdev <joseluismanco37@gmail.com>
2025-07-18 15:56:32 -0500

    fix: useLoadChains not mocked

--- src/hooks/loadables/useLoadChains.ts
+++ src/hooks/loadables/useLoadChains.ts
@@ -5,64 +5,17 @@ import {
   getChainsConfig,
   RPC_AUTHENTICATION,
   setBaseUrl,
+
   type ChainInfo,
 } from '@safe-global/safe-gateway-typescript-sdk'
 import useAsync, { type AsyncResult } from '../useAsync'
 import { logError, Errors } from '@/services/exceptions'
 
-const celoChain: ChainInfo = {
-  chainId: '42220',
-  chainName: 'Celo',
-  description: 'Celo mainnet',
-  chainLogoUri: '/images/currencies/celo.svg',
-  l2: false,
-  isTestnet: false,
-  nativeCurrency: {
-    name: 'CELO',
-    symbol: 'CELO',
-    decimals: 18,
-    logoUri: '/images/currencies/celo.svg',
-  },
-  transactionService: '',
-  blockExplorerUriTemplate: {
-    address: 'https://explorer.celo.org/address/{{address}}',
-    txHash: 'https://explorer.celo.org/tx/{{txHash}}',
-    api: '',
-  },
-  disabledWallets: [],
-  ensRegistryAddress: undefined,
-  features: [FEATURES.SAFE_APPS],
-  gasPrice: [
-    {
-      type: GAS_PRICE_TYPE.FIXED,
-      weiValue: '1000000000',
-    },
-  ],
-  publicRpcUri: {
-    authentication: RPC_AUTHENTICATION.NO_AUTHENTICATION,
-    value: 'https://forno.celo.org',
-  },
-  rpcUri: {
-    authentication: RPC_AUTHENTICATION.API_KEY_PATH,
-    value: 'https://celo-mainnet.infura.io/v3/',
-  },
-  safeAppsRpcUri: {
-    authentication: RPC_AUTHENTICATION.API_KEY_PATH,
-    value: 'https://celo-mainnet.infura.io/v3/',
-  },
-  shortName: 'celo',
-  theme: {
-    textColor: '#ffffff',
-    backgroundColor: '#35D07F',
-  },
-}
 
 const getConfigs = async (): Promise<ChainInfo[]> => {
-  setBaseUrl('https://safe-client.safe.global')
-  const data = await getChainsConfig()
-  const results = data.results || []
-  const hasCelo = results.some((c) => c.chainId === '42220')
-  return hasCelo ? results : [...results, celoChain]
+  const data = await fetch('https://safe-client.safe.global/v1/chains?cursor=limit=40')
+  const json = await data.json()
+  return json.results || []
 }
 
 export const useLoadChains = (): AsyncResult<ChainInfo[]> => {
