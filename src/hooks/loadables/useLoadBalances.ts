8db3dabf0e1f2212a5102803e4c42988ee88d775
sebaxor <49530370+sebaxor@users.noreply.github.com>
2025-08-13 22:06:20 -0500

    fix: lint fix

--- src/hooks/loadables/useLoadBalances.ts
+++ src/hooks/loadables/useLoadBalances.ts
@@ -54,7 +54,7 @@ export const useLoadBalances = (): AsyncResult<SafeBalanceResponse> => {
       })
       balances.fiatTotal = (
         Number(balances.fiatTotal) -
-        Number(balances.items.find((token) => token.tokenInfo.type === 'NATIVE_TOKEN')?.fiatBalance) || 0
+          Number(balances.items.find((token) => token.tokenInfo.type === 'NATIVE_TOKEN')?.fiatBalance) || 0
       ).toString()
 
       //Hotfix to avoid ERC-20 and CELO assets issue
