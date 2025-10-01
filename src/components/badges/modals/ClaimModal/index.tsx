5b2057714a999c4d9c231e250e5b85f07a6b5029
anotherdev <joseluismanco37@gmail.com>
2025-09-30 20:57:11 -0500

    feat: render strategies on claim

--- src/components/badges/modals/ClaimModal/index.tsx
+++ src/components/badges/modals/ClaimModal/index.tsx
@@ -8,6 +8,8 @@ import { useAppSelector } from '@/store'
 import { selectSuperChainAccount } from '@/store/superChainAccountSlice'
 import CheckCircleIcon from '@/public/images/common/check-circle.svg'
 import { GradientProgress } from '../..'
+import { getBadgeStrategy } from '../../badgeInfo/BadgeStrategyRenderer'
+import CeloVaultStrategy from '../../badgeInfo/strategies/CeloVaultStrategy'
 
 function ClaimModal({
   open,
@@ -39,6 +41,9 @@ function ClaimModal({
         .map((level) => {
           const badgeTier = updatedBadge.badgeTiers.find((tier: any) => Number(tier.metadata.level) === level)
           if (!badgeTier) return ''
+          const strategy = getBadgeStrategy(updatedBadge, [new CeloVaultStrategy()])
+          const customLabel = strategy?.formatTierLabel?.(updatedBadge as any, level, badgeTier)
+          if (customLabel) return customLabel
           return updatedBadge.metadata.condition.replace('{{variable}}', badgeTier.metadata.minValue)
         })
         .filter(Boolean)
